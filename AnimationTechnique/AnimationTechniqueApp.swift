71b72ff40672672e2f2aacee80bc634989237bc5
isiflo2728 <floresisidoro30@gmail.com>
2026-04-24 16:40:03 -0700

    Initial Commit

---
+++ AnimationTechnique/AnimationTechniqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTechniqueApp.swift
+//  AnimationTechnique
+//
+//  Created by Isidoro Flores on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTechniqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
