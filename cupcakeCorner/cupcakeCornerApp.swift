1e22b5a988d3eb2cc7e806f3040843dfa069b447
isiflo2728 <floresisidoro30@gmail.com>
2026-05-07 18:49:07 -0700

    Initial Commit

---
+++ cupcakeCorner/cupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cupcakeCornerApp.swift
+//  cupcakeCorner
+//
+//  Created by Isidoro Flores on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct cupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
