71bf9f97b99ddc39cc604af89c3072f66e7a3491
isiflo2728 <floresisidoro30@gmail.com>
2026-05-14 20:30:21 -0700

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Isidoro Flores on 5/14/26.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWormTests/BookWormTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormTests.swift
+//  BookWormTests
+//
+//  Created by Isidoro Flores on 5/14/26.
+//
+
+import Testing
+@testable import BookWorm
+
+struct BookWormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
