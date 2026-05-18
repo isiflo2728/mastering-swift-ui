8fb8514de777bdd792c08c69b121dce44c10b51a
isiflo2728 <floresisidoro30@gmail.com>
2026-05-18 12:21:35 -0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Isidoro Flores on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketListTests/BucketListTests.swift
@@ -0,0 +1,19 @@
+//
+//  BucketListTests.swift
+//  BucketListTests
+//
+//  Created by Isidoro Flores on 5/18/26.
+//
+
+import Testing
+@testable import BucketList
+
+struct BucketListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
