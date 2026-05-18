ada5fb1b48fbac575f5f1f589368d50aa3ea15e5
isiflo2728 <floresisidoro30@gmail.com>
2026-05-18 12:23:33 -0700

    Initial Commit

---
+++ instaFilter/instaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaFilterApp.swift
+//  instaFilter
+//
+//  Created by Isidoro Flores on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct instaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
