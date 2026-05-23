7cd4359516dabc6d344717e0927dedcfae5aab7c
isiflo2728 <floresisidoro30@gmail.com>
2026-05-22 18:14:35 -0700

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Isidoro Flores on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
