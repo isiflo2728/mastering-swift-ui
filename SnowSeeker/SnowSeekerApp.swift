aee187bd2e55b9d4e80166183ee93dd3df78d954
isiflo2728 <floresisidoro30@gmail.com>
2026-06-17 18:34:44 -0700

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Isidoro Flores on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
